6d7ef2a401795a2c6273c41cad2704c2d44e8c95
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-02-08 15:47:05 +0530

    added access to the new frontend

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,32 @@
-// const express = require('express')// method-1
-import express from "express"; // method-2
-import dotenv from "dotenv"; 
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
-dotenv.config({});
+import { app, server } from "./socket/socket.js";
 
- 
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
-// middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
+
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://my-chat-app-one-orcin.vercel.app"],
+    credentials: true
 };
-app.use(cors(corsOption)); 
+app.use(cors(corsOptions));
 
+// Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
-// routes
-app.use("/api/v1/user",userRoute); 
-app.use("/api/v1/message",messageRoute);
- 
-
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
     connectDB();
-    console.log(`Server listen at prot ${PORT}`);
+    console.log(`Server listening at port ${PORT}`);
 });
-
